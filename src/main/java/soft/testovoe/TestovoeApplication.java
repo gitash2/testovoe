206d4c618d51d3d3eafebe669f46d6bea6816ada
gitash2 <shapor.art@yandex.ru>
2025-11-10 15:00:20 +0300

    fix version

--- src/main/java/soft/testovoe/TestovoeApplication.java
+++ src/main/java/soft/testovoe/TestovoeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestovoeApplication {
-    void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(TestovoeApplication.class, args);
     }
 }
